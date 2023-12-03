da0a618c3256950a4116e83b4f8e631d2442ef79
OceanForm <oceanform@icloud.com>
2023-12-03 16:44:23 +0100

    Initial commit

---
+++ AribnbSwiftUI/AribnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AribnbSwiftUIApp.swift
+//  AribnbSwiftUI
+//
+//  Created by Slava Medvedskyi on 03/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AribnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
